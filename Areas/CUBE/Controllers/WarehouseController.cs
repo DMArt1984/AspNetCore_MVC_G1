1fb2fc2bea36f0d01828d1beec420e66878f9a7e
DMArt1984 <dmxart13@gmail.com>
2025-03-08 11:48:08 +0600

    Areas.CUBE

---
+++ Areas/CUBE/Controllers/WarehouseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_MVC_Project.Areas.CUBE.Controllers
+{
+    public class WarehouseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
