9c04cff64707521286154d3c7ee35981ce4a3037
DMArt1984 <dmxart13@gmail.com>
2025-03-08 11:41:22 +0600

    Areas.BOX > ToiR, Market

---
+++ Areas/BOX/Controllers/MarketController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_MVC_Project.Areas.BOX.Controllers
+{
+    public class MarketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
