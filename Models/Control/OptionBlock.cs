82231e2be305b099ba1e0c273acd5d2fc3ab2097
DMArt1984 <dmxart13@gmail.com>
2025-03-25 08:54:55 +0500

    class OptionBlock > add NameArea

--- Models/Control/OptionBlock.cs
+++ Models/Control/OptionBlock.cs
@@ -14,6 +14,12 @@
         public string NameController { get; set; }
 
         /// <summary>
+        /// Название области, к которой разрешен доступ.
+        /// Например, "BOX" или "CUBE".
+        /// </summary>
+        public string NameArea { get; set; }
+
+        /// <summary>
         /// Навигационное свойство - список покупок, связанных с этим блоком.
         /// </summary>
         public virtual ICollection<Purchase> Purchases { get; set; } = new List<Purchase>();
